bb3cd8d43891163704698156a82e8b228f9fb284
Zhao Qi <qi.zhao1991@gmail.com>
2019-11-05 20:48:28 +0100

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -31,6 +31,7 @@ class Util:
         """
         Util.random_sets = {}
 
+
 def tensor_to_matrix(inputs, ksize, stride=1, pad=0):
     """Reshape tensor of shape [batch_size, channel, height, width] into matrix with shape [batch_size*output_h*output_w, channel*kernel_h*kernel_w] 
    
@@ -48,13 +49,28 @@ def tensor_to_matrix(inputs, ksize, stride=1, pad=0):
     Returns:
         2D matrix with same type data that tensor of shape [batch_size*output_h*output_w, channel*kernel_h*kernel_w] 
     """
+    if len(inputs.shape) != 4:
+        raise ValueError('The shape of input tensor must be [inputs_nums, channel, height, width]')
+    
+    if len(ksize) != 2:
+        raise ValueError('Kernel size must be a list of shape [kernel_h, kernel_w]')
+
+    if not isinstance(ksize, (tuple, list)):
+        ksize = [ksize]
+
+    if not isinstance(stride, int):
+        stride = int(stride)
+    
+    if not isinstance(pad, int):
+        pad = int(pad)
+
     inputs_nums, inputs_c, inputs_h, inputs_w = inputs.shape
     kernel_h, kernel_w = ksize
     # calculate output shape: output_h and output_w
     output_h = (inputs_h + pad*2 - kernel_h) // stride + 1
     output_w = (inputs_w + pad*2 - kernel_w) // stride + 1
 
-    # define tensor and its return
+    # define tensor and matrix
     tensor = np.pad(inputs, pad_width=[(0, 0), (0, 0), (pad, pad), (pad, pad)], mode='constant')
     matrix = np.zeros((inputs_nums, inputs_c, kernel_h, kernel_w, output_h, output_w), dtype=inputs.dtype)
 
@@ -70,21 +86,36 @@ def tensor_to_matrix(inputs, ksize, stride=1, pad=0):
 
 def matrix_to_tensor(inputs, shape, ksize, stride=1, pad=0):
     """Reshape matrix of shape  [batch_size*out_h, batch_size*out_w] into tensor with shape [batch_size, channel, height, width] 
-    You can think of this method as the reverse function of im2col. It is adding up the corresponing indices and transforms the given matrix
+    You can think of this method as the reverse function of tensor_to_matrix. It is adding up the corresponing indices and transforms the given matrix
     back into the initial shape.
 
-    Assuming you have a im2col transformed matrix with shape (9,600*13*13). The original image had a shape of (600,1,28,28) with padding P = 0 
+    Assuming you have a matrix_to_tensor transformed matrix with shape (9,600*13*13). The original image had a shape of (600,1,28,28) with padding P = 0 
     and stride S = 2. col2im creates out of the im2col matrix and the same hyperparameter a new matrix with a shape of (600, 1, 28, 28).
 
     Args:
         inputs: 2D matrix of shape [batch_size*output_h*output_w, batch_size*kernel_h*kernel_w] 
         shape: int list, tensor of shape [batch_size, channel, height, width]
         ksize: int list, filter shape of [kernel_h, kernel_w] 
-        stride: int, he filter convolves around the input volume by shifting one unit at a time, default value 1
+        stride: int, the filter convolves around the input volume by shifting one unit at a time, default value 1
         pad: int, zero padding pads the input volume with zeros around the border, default value 0
     Returns:
         4D tensor of shape [batch_size, channel, height, width]
     """
+    if len(inputs.shape) != 4:
+        raise ValueError('The shape of input tensor must be [inputs_nums, channel, height, width]')
+
+    if len(ksize) != 2:
+        raise ValueError('Kernel size must be a list of shape [kernel_h, kernel_w]')
+    
+    if not isinstance(ksize, (tuple, list)):
+        ksize = [ksize]
+
+    if not isinstance(stride, int):
+        stride = int(stride)
+    
+    if not isinstance(pad, int):
+        pad = int(pad)
+
     # get inputs 4d tensor shape
     inputs_nums, inputs_c, inputs_h, inputs_w = shape
     kernel_h, kernel_w = ksize
@@ -103,6 +134,65 @@ def matrix_to_tensor(inputs, shape, ksize, stride=1, pad=0):
             tensor[:, :, y:y_max:stride, x:x_max:stride] += matrix[:, :, y, x, :, :]
 
     return tensor[:, :, pad:inputs_h + pad, pad:inputs_w + pad]
+
+
+def get_conv_output_shape(inputs_h, inputs_w, ksize, stride=1, pad=0):
+    """compute convolution's shape
+    Args:
+        inputs_h: int, inputs tensor height
+        inputs_w: int, inputs width
+        ksize: int list, filter shape of [kernel_h, kernel_w]
+        stride: int, the filter convolves around the input volume by shifting one unit at a time, default value 1
+        pad: int, zero padding numbers
+    Returns:
+        outputs height and output width
+    """
+
+    if len(ksize) != 2:
+        raise ValueError('Kernel size must be a list of shape [kernel_h, kernel_w]')
+
+    if not isinstance(inputs_h, int):
+        inputs_h = int(inputs_h)
+    
+    if not isinstance(inputs_w, int):
+        inputs_w = int(inputs_w)
+    
+    if not isinstance(ksize, (tuple, list)):
+        ksize = [ksize]
+    
+    if not isinstance(stride, int):
+        stride = int(stride)
+    
+    if not isinstance(pad, int):
+        pad = int(pad)
+
+    kernel_h, kernel_w = ksize
+    # calculate output height/width
+    output_h = int((inputs_h + pad*2 - kernel_h) / stride) + 1
+    output_w = int((inputs_w + pad*2 - kernel_w) / stride) + 1
+    return (output_h, output_w)
+
+def one_hot_encoder(y, label_nums=None):
+    """One hot encoding method, Categorical data must be converted be number data.A one hot encoding is a representation of categorical variables as binary vectors.
+    data, This first requires that the categorical values be mapped to integer values. Then, each integer value is represented as a binary vector that is all 
+    zero values except the index of the integer, which is marked with a 1.
+    Args:
+        y: predicting target 
+        label_nums: the number of labels
+    Returns:
+        one hot encoded label
+    """
+    if label_nums is not None:
+        label_nums = label_nums
+    else:
+        label_nums = np.max(y) + 1
+    
+    one_hot = np.zeros((y.shape[0], label_nums), dtype=np.int)
+    one_hot[np.arange(y.shape[0]), y] = 1
+    return one_hot
+
+
+
 
 
 
