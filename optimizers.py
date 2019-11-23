669034589a22e593fe6dc6d06693d07cdee5e233
Zhao Qi <qi.zhao1991@gmail.com>
2019-11-23 20:55:46 +0100

    Update optimizers.py

--- optimizers.py
+++ optimizers.py
@@ -48,3 +48,33 @@ class SGD(Optimizer):
     def get_params(self):
         params = {'learning_rate': float(self.learning_rate)}
         return list(params)
+
+class Momentum(Optimizer):
+    """Momentum method helps accelerate SGD relevant direction. It does this by adding a fraction momemtum of the updated vector 
+    of the past time step to the current update vector.
+
+    v = -lr * dx + v * momemtum
+    """
+    def __init__(self, learning_rate=0.1, momemtum=0.0):
+        super(Momentum, self).__init__()
+        self.learning_rate = learning_rate
+        self.momentum = momentum
+        self.v = None
+
+    def update_layer(self):
+        if self.v is None:
+            self.v = {} # strore update vals
+        for i, layer in enumerate(self.layers):
+            self.v[i] = {}
+            for j, param in enumerate(layer):
+                self.v[i][j] = np.zeros_like(param.data)
+
+        for i, layer in enumerate(self.layers):
+            for j, param in enumerate(layer):
+                self.v[i][j] = self.momentum * self.v[i][j] + (1 - self.momentum) * param.grad.data
+                param.data -= self.learning_rate * self.v[i][j]
+				param.grad.data *= 0
+    def get_params(self):
+        params = {'learning_rate': float(self.learning_rate), 
+                  'momentum': float(self.momentum)}
+        return list(params)
