89a56f94c69a88a6771ce8fcde6f5bf83694f6d8
Zhao Qi <qi.zhao1991@gmail.com>
2019-11-24 21:01:07 +0100

    Update losses.py

--- losses.py
+++ losses.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from tensor import Tensor
+from tensors import Tensor
 from .utils.math_ops_utils import *
 
 
