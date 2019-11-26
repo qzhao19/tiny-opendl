3f41c55e26f2988458c2283789cafa6d09f97257
Zhao Qi <qi.zhao1991@gmail.com>
2019-11-26 22:05:10 +0100

    Create losses_unit_test.py

---
+++ unit_test/losses_unit_test.py
@@ -0,0 +1,14 @@
+from .tensors import Tensor
+from .losses
+
+
+a = np.random.randint(0, 100, (3, 3))
+b = np.random.randint(0, 100, (3, 3))
+a_t = Tensor(np.random.random((3, 3)), auto_grad=True)
+b_t = Tensor(np.random.random((3, 3)), auto_grad=True)
+
+# np.argmax(a_t.data, 1)
+print(a_t.data)
+print(b_t.data)
+
+SoftmaxCrossEntropy().backward(a_t, b_t)
