991387fac985ba4e64ad05aa4b4ed6852d22c1cd
Zhao Qi <qi.zhao1991@gmail.com>
2019-11-26 22:01:49 +0100

    Update losses.py

--- losses.py
+++ losses.py
@@ -9,7 +9,7 @@ from .utils.math_ops_utils import *
 class Loss(object):
     """Loss base class. To be implemented by sub-classes 'backward'
     """
-    def backward(self, y_true, y_prde):
+    def backward(self, y_true, y_pred):
         """Back propagatation from loss function
 
         Args:
@@ -24,8 +24,12 @@ class Loss(object):
         if y_true.data.shape != y_pred.data.shape:
             raise ValueError('The y_true should be same shape that y_pred')
 
-    def acc(self, y_true, y_pred):
-        pass
+    def compute_acc(self, y_true, y_pred):
+        y_true_arg = np.argmax(y_true, axis=1)
+        y_pred_arg = np.argmax(y_pred, axis=1)
+
+        # true_pred_arg =  np.sum(y_true_arg == y_pred_arg)
+        return np.sum(y_true_arg == y_pred_arg) / len(y_true_arg)
 
 
 class MSE(Loss):
@@ -34,24 +38,41 @@ class MSE(Loss):
     """
     def backward(self, y_true, y_pred):
         """Sub-class of backward of Loss, back propagatation from loss function
+        Args:
+            y_true: Tensor, labels
+            y_pred: Tensor, logits
         """
         # inhrited from Loss
-        super().backward(y_treu, y_pred)
+        super().backward(y_true, y_pred)
         loss = (y_pred - y_true).__power__(2).sum(0)
         error = loss.data.sum()
-        acc = self.acc(y_true, y_pred)
+
+        acc = self.compute_acc(y_true.data, y_pred.data)
 
         loss.backward(Tensor(np.ones_like(loss.data)))
 
-        return error, 
-
-    def acc(self, y_true, y_pred):
-        labels = np.argmax(y_true, axis=1)
-        logits = np.argmax(y_pred, axix=1)
-
-        true_preds =  np.sum(labels == logits)
-        return true_pred / len(labels)
+        return error, acc
 
 
+class SoftmaxCrossEntropy(Loss):
+    """Computes softmax cross entropy between `logits` and `labels`.
+        
+    Measures the probability error in discrete classification tasks in which the
+    classes are mutually exclusive (each entry is in exactly one class).
+    """
+    def backward(self, y_true, y_pred):
+        """softmax cross entropy
+        """
+        super().backward(y_true, y_pred)
+        # transform logtis vals into probabilities
+        logits = softmax(y_pred.data)
 
+        loss = y_pred.cross_entropy(y_true)
+        error = loss.data.sum()
+
+        acc = self.compute_acc(y_true.data, y_pred.data)
+
+        loss.backward(Tensor(np.ones_like(loss.data)))
+
+        return error, acc
 
