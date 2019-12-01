d63a22b5498b3ed603a4908cd60d94f03789a10a
Zhao Qi <qi.zhao1991@gmail.com>
2019-12-01 21:38:58 +0100

    Update inputs.py

--- inputs.py
+++ inputs.py
@@ -4,12 +4,14 @@ from __future__ import print_function
 
 
 class Input(object):
-    """A placeholder class for a tensor that be fed 
+    """A placeholder class for a tensor that be fed. Layer to be used as an entry point into a Network.
+    This class can create placeholders for Tensors
     
     Args:
-        shape: int list, the shape of the tensor to be fed
-        input_pl: the placeholder to be input
-        input_layer: tensor, input layer
+        shape: int list, the shape of the tensor to be fed, 
+               A shape tuple, not including the batch size.
+        input_pl: the input placeholder to take the last layer placeholder
+        input_layer: tensor, input tensor
     
     Methods:
         set_name: 
@@ -19,19 +21,20 @@ class Input(object):
         set_input_layer:
     """
     def __init__(self, shape=None, input_pl=None, input_layer=None):
-        self.name = None
-        # self.tensor = None
+        # An optional name string for the layer.
+        self.id = None
+        self.tensor = None
         if shape is None and input_pl is None:
             raise ValueError('Provide the input shape and input placeholder')
         self.shape = shape
         self.input_pl = input_pl
         self.input_layer = input_layer
 
-    def set_name(self, name):
-        self.name = name
+    def set_id(self, id):
+        self.id = id
     
-    # def set_tensor(self, tensor):
-    #     self.tensor = tensor
+    def set_tensor(self, tensor):
+        self.tensor = tensor
     
     def set_shape(self, shape):
         self.shape = shape
