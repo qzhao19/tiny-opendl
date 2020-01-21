eaba14a1416996c05c89f8fdfd7ae6cf64fab85e
Zhao Qi <qi.zhao1991@gmail.com>
2020-01-21 22:15:50 +0100

    Update model.py

--- model.py
+++ model.py
@@ -1,3 +1,7 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 from inputs import *
 from losses import *
 from optimizers import *
